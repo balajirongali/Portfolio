e195861d22cc8d09ac8a62965ec573da041d0d18
balajirongali <balajirongali0504@gmail.com>
2025-06-15 21:13:43 +0530

    added website link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,28 +7,30 @@ const projects = [
     image: "/attendance.jpeg",
     description: 'This project implements a Siamese Neural Network for face recognition to automate attendance marking. The system captures facial images, processes them, and verifies identities using a deep learning model.',
     tech: ['OpenCV', 'Siamese Networks', 'CNN', 'Deep Learning'],
-    link: 'https://github.com/balajirongali/Attendance-Recorder'
+    gitlink: 'https://github.com/balajirongali/Attendance-Recorder'
   },
   {
     title: 'iNotebook-A Note taking App',
     image:"/inotebook.jpg",
-    description: 'Built a secure and responsive full-stack note-taking app using the MERN stack. Integrated JWT-based authentication, CRUD operations via Fetch API, and a clean UI with React Router for seamless user experience.',
+    description: 'Built a secure and responsive full-stack note-taking app using the MERN stack. Integrated JWT-based authentication,Email OTP , CRUD operations via Fetch API, and a clean UI with React Router for seamless user experience.',
     tech: ['FullStack', 'React', 'MongoDb','Express Js','JWT Token'],
-    link: 'https://github.com/balajirongali/iNotebook'
+    gitlink: 'https://github.com/balajirongali/iNotebook',
+    link:'https://inotebook-lilac-eight.vercel.app/'
+
   },
   {
     title: 'NewsMonkey',
     image:"/newsmonkey.jpg",
     description: 'Developed a dynamic news aggregator using React, NewsAPI, and Bootstrap, featuring infinite scroll and category-based filtering. Optimized API usage for faster load times and ensured smooth user experience with efficient state management using React Hooks.',
     tech: ['NewsApi', 'React','Bootstrap'],
-    link: 'https://github.com/balajirongali/NewsMonkey'
+    gitlink: 'https://github.com/balajirongali/NewsMonkey'
   },
   {
     title: 'Bank Management System',
     image:"/bank.jpeg",
     description: 'Created a modular Bank Management System in C++ using Object-Oriented Programming principles. Implemented features like account creation, updates, deposits, and withdrawals. Leveraged inheritance and polymorphism to ensure scalability and maintainability.',
     tech: ['OOPs', 'C++','Console-based UI'],
-    link: 'https://github.com/balajirongali/bank_management_system'
+    gitlink: 'https://github.com/balajirongali/bank_management_system'
   },
   // Add more projects as needed
 ];
@@ -49,7 +51,16 @@ function Projects() {
                 <span className="tech-tag" key={idx}>{tech}</span>
               ))}
             </div>
-            <button className='btn btn-primary'><a href={project.link} className="project-link" target="_blank" rel="noopener noreferrer">Github</a></button>
+            <div>
+            <button className='btn btn-primary'><a href={project.gitlink} className="project-link" target="_blank" rel="noopener noreferrer">Github</a></button>
+            {project.link && (
+            <button style={{margin:'4px'}} className='btn btn-success'>
+              <a href={project.link} className="project-link" target="_blank" rel="noopener noreferrer">
+                View
+              </a>
+            </button>
+          )}
+          </div>
           </div>
         ))}
       </div>
